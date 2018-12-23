9851da0d6ba58fee7a7ff849b17abd4f7e77f859
Matsan Yevhenii <matsan_e_m@yahoo.com>
2018-12-23 18:51:00 +0200

    added login query   Changes to be committed: 	modified:   app/Controllers/Http/AuthController.js

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,6 +1,29 @@
+const Database = use('Database');
+
 class AuthController {
-  async login() {
-    return [{ key: 1 }, { key: 2 }, { key: 3 }];
+  async login({ request }) {
+    const indata = request.only(['user', 'pass']);
+    /*  const user = await Database.table('tuser')
+      .select('fiduser')
+      .where('fuser', '=', `${indata.user}`)
+      .andWhere('fpassword', `${indata.pass}`); */
+    /*  const user = await Database.select('fiduser', 'fidrole', 'frole')
+      .from('tuser', 'trole')
+      .where('tuser.fidrole_fk', 'trole.fidrole')
+      .where('fuser', '=', `${indata.user}`)
+      .andWhere('fpassword', `${indata.pass}`); */
+    const user = await Database.select('*')
+      .from('tuser')
+      .leftJoin('trole', 'tuser.fidrole_fk', 'trole.fidrole')
+      .where('fuser', '=', `${indata.user}`)
+      .andWhere('fpassword', `${indata.pass}`);
+    /* if (user[0]) {
+      return true;
+    }
+    return false; */
+    // return user;
+    if (user[0]) return user[0].frole;
+    return 'anonimous';
   }
 }
 
